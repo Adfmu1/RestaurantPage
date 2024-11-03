2dda3ee6b5f73f87cc232534a01022c16c328c04
Adfmu <Adfmu3@gmail.com>
2024-11-03 20:01:11 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+import { initialPageLoad } from "./initial-load.js"
+
+initialPageLoad();
+
---
+++ src/initial-load.js
@@ -0,0 +1,11 @@
+const contentDiv = document.querySelector("#content")
+
+export function initialPageLoad(){
+    const mainHeader1 = document.createElement("h1")
+    mainHeader1.textContent = "Hello there";
+    contentDiv.appendChild(mainHeader1);
+
+
+    contentDiv.appendChild()
+
+}
