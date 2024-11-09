3238380c42eaf67d5e0a94f0aeab64f525c7fe4b
Adfmu <Adfmu3@gmail.com>
2024-11-09 01:14:48 +0100

    add and style beverages the same way as pizzas

--- src/menu-load.js
+++ src/menu-load.js
@@ -14,8 +14,8 @@ export function menuPageLoad(){
     pizzasDiv.appendChild(pizzasHeader);
 
     const pizzas = ["Margherita", "Capriciosa", "Vesovio"];
-    const prices = [10, 13, 15];
-    const descriptions = ["Good ol' cheesy pizza", "Margherita but with shrooms and ham", "DOUBLE HAM!"];
+    const pricesPizzas = [10, 13, 15];
+    const descriptionsPizzas = ["Good ol' cheesy pizza", "Margherita but with shrooms and ham", "DOUBLE HAM!"];
 
     for (let i = 0; i < pizzas.length; i++) {
         const pizzaDiv = document.createElement("div")
@@ -27,10 +27,10 @@ export function menuPageLoad(){
         pizzaName.textContent = pizzas[i];
 
         const pizzaPrice = document.createElement("p");
-        pizzaPrice.textContent = prices[i];
+        pizzaPrice.textContent = pricesPizzas[i].toString();
 
         const pizzaDescription = document.createElement("p");
-        pizzaDescription.textContent = descriptions[i];        
+        pizzaDescription.textContent = descriptionsPizzas[i];
 
         pizzaDiv.appendChild(pizzaName);
         pizzaDiv.appendChild(pizzaPrice);
@@ -40,5 +40,41 @@ export function menuPageLoad(){
     }
 
     contentDiv.appendChild(pizzasDiv);
+
+    const beveragesMenu = document.createElement("div");
+    beveragesMenu.id = "beverages-div";
+
+    const beveragesHeader = document.createElement("h3");
+    beveragesHeader.textContent = "Beverages";
+    beveragesHeader.id = "beverages-header";
+    beveragesMenu.appendChild(beveragesHeader);
+
+    const beverages = ["Water", "Coca-cola", "Mojito"];
+    const pricesBeverages = [2, 5, 7];
+    const descriptionsBeverages = ["Plain water", "Good old coke", "Refreshing lime, lemon and mint mixed together"];
+
+    for (let i = 0; i < beverages.length; i++) {
+        const beverageDiv = document.createElement("div")
+        beverageDiv.id = beverages[i].toLowerCase();
+
+        beverageDiv.classList.add("beverage");
+
+        const beverageName = document.createElement("h4");
+        beverageName.textContent = beverages[i];
+
+        const beveragePrice = document.createElement("p");
+        beveragePrice.textContent = pricesBeverages[i].toString();
+
+        const beverageDescription = document.createElement("p");
+        beverageDescription.textContent = descriptionsBeverages[i];
+
+        beverageDiv.appendChild(beverageName);
+        beverageDiv.appendChild(beveragePrice);
+        beverageDiv.appendChild(beverageDescription);
+
+        beveragesMenu.appendChild(beverageDiv);
+    }
+
+    contentDiv.appendChild(beveragesMenu);
 
 }
